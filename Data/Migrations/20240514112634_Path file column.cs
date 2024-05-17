78e66da1c523d3f3b0847ae9fdc706991b426655
Farangis Ölund <bless.faro@gmail.com>
2024-05-17 22:13:33 +0200

    Add project files.

---
+++ Data/Migrations/20240514112634_Path file column.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Pathfilecolumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PathName",
+                table: "Files",
+                newName: "FilePath");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FilePath",
+                table: "Files",
+                newName: "PathName");
+        }
+    }
+}
---
+++ FileProvider/Program.cs
@@ -0,0 +1,29 @@
+using Azure.Storage.Blobs;
+using Data.Contexts;
+using FileProvider.Services;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder()
+	.ConfigureFunctionsWebApplication()
+	.ConfigureServices(services =>
+	{
+		services.AddApplicationInsightsTelemetryWorkerService();
+		services.ConfigureFunctionsApplicationInsights();
+		services.AddDbContext<DataContext>(x => x.UseSqlServer(Environment.GetEnvironmentVariable("SqlServer")));
+		
+		services.AddScoped<BlobServiceClient>(x => new BlobServiceClient(Environment.GetEnvironmentVariable("Azure_Storage_Account")));
+
+		services.AddSingleton<BlobContainerClient>(x =>
+		{
+			var blobServiceClient = x.GetRequiredService<BlobServiceClient>();
+			return blobServiceClient.GetBlobContainerClient("default-container");
+		});
+
+		services.AddScoped<FileService>();
+	})
+	.Build();
+
+host.Run();
